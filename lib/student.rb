5316760aa447db2a3fe7965827b5539deec7aa01
Pablo Gonzalez <pabgon0@gmail.com>
2020-04-22 21:55:59 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -33,6 +33,75 @@ class Student
     end.first
   end
   
+   def self.all_students_in_grade_9
+    sql = <<-SQL
+      SELECT *
+      FROM students
+      WHERE grade = 9
+      LIMIT 1
+    SQL
+    
+    DB[:conn].execute(sql).map do |row|
+      array = self.new_from_db(row)
+      array
+    end
+  end
+  
+  def self.students_below_12th_grade
+    sql = <<-SQL
+      SELECT *
+      FROM students
+      WHERE grade < 12
+      LIMIT 1
+    SQL
+    
+    DB[:conn].execute(sql).map do |row|
+      array = self.new_from_db(row)
+      array
+    end
+  end
+  
+  def self.first_X_students_in_grade_10(x)
+    sql = <<-SQL
+      SELECT *
+      FROM students
+      WHERE grade = 10
+      LIMIT ?
+    SQL
+    
+    DB[:conn].execute(sql, x).map do |row|
+      array = self.new_from_db(row)
+      array
+    end
+  end
+  
+  def self.first_student_in_grade_10
+    sql = <<-SQL
+      SELECT *
+      FROM students
+      WHERE grade = 10
+      LIMIT 1
+    SQL
+    
+    DB[:conn].execute(sql).map do |row|
+      array = self.new_from_db(row)
+      array
+    end.first
+  end
+  
+  def self.all_students_in_grade_X(x)
+    sql = <<-SQL
+      SELECT *
+      FROM students
+      WHERE grade = ?
+    SQL
+    
+    DB[:conn].execute(sql, x).map do |row|
+      array = self.new_from_db(row)
+      array
+    end
+  end
+  
   def save
     sql = <<-SQL
       INSERT INTO students (name, grade) 
